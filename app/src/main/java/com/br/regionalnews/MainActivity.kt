f0821a14063af3c33399891ae17ab1d11c82b1bf
Victor Lucas <victorlucas@alu.ufc.br>
2019-10-24 13:53:04 -0300

    Registro de activity articles list na activity main

--- app/src/main/java/com/br/regionalnews/MainActivity.kt
+++ app/src/main/java/com/br/regionalnews/MainActivity.kt
@@ -6,47 +6,40 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.viewpager.widget.ViewPager
+import com.br.regionalnews.model.Article
+import com.br.regionalnews.view.article.ArticleAdapter
 import com.br.regionalnews.view.configuration.ConfigurationActivity
 import com.br.regionalnews.view.initial.InitialPageAdapter
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_articles_list.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_articles_list)
 
-        val fragmentAdapter = InitialPageAdapter(supportFragmentManager)
+        val recycleView = recyclerView
 
-        pager.adapter = fragmentAdapter
 
-        tabs_main.setupWithViewPager(pager)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ArticleAdapter(getArticles())
+        }
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
-        return true
-    }
+    private fun getArticles(): ArrayList<Article> {
+        val dataList = ArrayList<Article>()
+        for (i in 1..10){
+            dataList.add(Article("Notícia #"+i, "Uma breve descrição da notícia#"+i))
+        }
 
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_help -> {
-            msgShow("RegionalNews, conectando você com as notícias da sua região!")
-            true
-        }
-        R.id.action_setting -> {
-            val configuracoes = Intent(this, ConfigurationActivity::class.java)
-            startActivity(configuracoes)
-            true
-        }
-        else -> {
-            super.onOptionsItemSelected(item)
-        }
-    }
-
-    fun msgShow(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        return dataList;
     }
 }
