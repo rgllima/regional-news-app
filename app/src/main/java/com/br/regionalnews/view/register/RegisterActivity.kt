84d82b8f5a8b7e6cfc720777d32bf22ebed044aa
Rafael Lima <rglllima@gmail.com>
2019-10-24 19:13:02 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/main/java/com/br/regionalnews/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.br.regionalnews.view.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.br.regionalnews.R
+
+class RegisterActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
