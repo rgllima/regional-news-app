b1a7555b47bb381dcd2a48c7f37aec99d6860c00
Victor Lucas <victorlucas@alu.ufc.br>
2019-11-30 14:43:36 -0300

    Factory ArticleAuthorsViewModel

---
+++ app/src/main/java/com/br/regionalnews/factories/ArticleAuthorsViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.br.regionalnews.factories
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.br.regionalnews.repository.ArticleRepository
+import com.br.regionalnews.viewmodel.ArticleAuthorsViewModel
+
+class ArticleAuthorsViewModelFactory constructor(private val repository: ArticleRepository,
+                                                 private val application: Application): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        with(modelClass){
+            when {
+                isAssignableFrom(ArticleAuthorsViewModel::class.java) ->
+                    ArticleAuthorsViewModel(repository, application)
+                else ->
+                    throw IllegalArgumentException("Classe desconhecida.")
+            }
+        } as T
+}
