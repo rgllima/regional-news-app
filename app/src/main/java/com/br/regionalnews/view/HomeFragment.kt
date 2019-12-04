271a1ac7b04bb5a000c165405ed3a09d7b6a0acb
Victor Lucas <victorlucas@alu.ufc.br>
2019-12-04 10:21:49 -0300

    Atualização em localização e uso de notícias em horizontal no HomeFrag

--- app/src/main/java/com/br/regionalnews/view/HomeFragment.kt
+++ app/src/main/java/com/br/regionalnews/view/HomeFragment.kt
@@ -1,21 +1,26 @@
-package com.br.regionalnews.view.home
+package com.br.regionalnews.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.br.regionalnews.R
 import com.br.regionalnews.config.ApiResponse
 import com.br.regionalnews.config.RetrofitInstance
+import com.br.regionalnews.factories.ArticleWriterViewModelFactory
+import com.br.regionalnews.factories.HomeViewModelFactory
 import com.br.regionalnews.model.Article
+import com.br.regionalnews.repository.ArticleRepository
 import com.br.regionalnews.view.article.ArticleAdapter
+import com.br.regionalnews.view.article.ArticleHorizontalAdapter
+import com.br.regionalnews.viewmodel.ArticleWriterViewModel
+import com.br.regionalnews.viewmodel.HomeViewModel
 import kotlinx.android.synthetic.main.fragment_home.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,11 +28,24 @@ import retrofit2.Response
 
 class HomeFragment: Fragment() {
 
+    lateinit var viewModel: HomeViewModel
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        getArticles()
         initButtons()
+
+        viewModel = createViewModel()
+
+        viewModel.getArticles()
+
+        viewModel.articles.observe(this, Observer {
+            configureListVertical(viewModel.articles.value!!)
+        })
+
+        viewModel.articlesHorizontal.observe(this, Observer {
+            configureListHorizontal(viewModel.articlesHorizontal.value!!)
+        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -35,10 +53,17 @@ class HomeFragment: Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    private fun configureList(articles: ArrayList<Article>) {
+    private fun configureListVertical(articles: List<Article>) {
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = ArticleAdapter(articles)
+        }
+    }
+
+    private fun configureListHorizontal(articles: List<Article>) {
+        recyclerViewHorizontal.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = ArticleHorizontalAdapter(articles)
         }
     }
 
@@ -56,24 +81,9 @@ class HomeFragment: Fragment() {
         }
     }
 
-    private fun getArticles() {
+    private fun createViewModel(): HomeViewModel {
+        val factory = HomeViewModelFactory(ArticleRepository, activity?.application!!)
 
-        val call = RetrofitInstance.articleService().listArticles()
-
-        call.enqueue(object: Callback<ApiResponse?> {
-            override fun onResponse(call: Call<ApiResponse?>, response: Response<ApiResponse?>?) {
-                response?.body()?.let {
-
-//                    Toast.makeText(context, "Requisição", Toast.LENGTH_SHORT).show()
-                    val apiResponse: ApiResponse = it
-                    configureList(apiResponse.data.list)
-
-                }
-            }
-
-            override fun onFailure(call: Call<ApiResponse?>, t: Throwable) {
-                Toast.makeText(context, "Ocorreu um erro! ${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
+        return ViewModelProviders.of(this, factory).get(HomeViewModel::class.java)
     }
 }
