5294ed71ea18d46de0a77bbd88887c40093f7951
Victor Lucas <victorlucas@alu.ufc.br>
2019-12-04 09:36:20 -0300

    Atualização em ArticleRepository

--- app/src/main/java/com/br/regionalnews/repository/ArticleRepository.kt
+++ app/src/main/java/com/br/regionalnews/repository/ArticleRepository.kt
@@ -1,5 +1,6 @@
 package com.br.regionalnews.repository
 
+import com.br.regionalnews.config.SaveArticleRequest
 import com.br.regionalnews.datasource.ArticleDataSource
 import com.br.regionalnews.model.Article
 
@@ -7,10 +8,34 @@ object ArticleRepository {
     private val dataSource = ArticleDataSource
 
     fun listAll(success: (List<Article>) -> Unit, failure: () -> Unit) {
+        dataSource.listLastArticles({
+            success(it)
+        }, {
+            dataSource.listLastArticles(success, failure)
+        })
+    }
+
+    fun listAllByUser(success: (List<Article>) -> Unit, failure: () -> Unit) {
         dataSource.listArticleByUser({
             success(it)
         }, {
             dataSource.listArticleByUser(success, failure)
+        })
+    }
+
+    fun save(article: SaveArticleRequest, success: (Article) -> Unit, failure: () -> Unit){
+        dataSource.save(article, {
+            success(it)
+        }, {
+            dataSource.save(article, success, failure)
+        })
+    }
+
+    fun update(article: SaveArticleRequest, success: (Article) -> Unit, failure: () -> Unit){
+        dataSource.update(article, {
+            success(it)
+        }, {
+            dataSource.update(article, success, failure)
         })
     }
 
