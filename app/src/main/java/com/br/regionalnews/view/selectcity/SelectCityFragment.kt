2267b78638670522d9ec88b38cbf3d08dc12a3b2
Victor Lucas <victorlucas@alu.ufc.br>
2019-12-08 20:47:23 -0300

    Adição de localização atual do usuário

--- app/src/main/java/com/br/regionalnews/view/selectcity/SelectCityFragment.kt
+++ app/src/main/java/com/br/regionalnews/view/selectcity/SelectCityFragment.kt
@@ -1,12 +1,27 @@
 package com.br.regionalnews.view.selectcity
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.br.regionalnews.R
+import com.br.regionalnews.view.ArticleWriterFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,17 +31,24 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_select_city.*
 
 class SelectCityFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val mapFragment = fragmentManager!!
-            .findFragmentById(R.id.mapView) as SupportMapFragment?
-
-        mapFragment?.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val mapFragment = childFragmentManager!!
+            .findFragmentById(R.id.mapView) as SupportMapFragment?
+
+        mapFragment?.getMapAsync(this)
+
+
 
         back_button.setOnClickListener{
             findNavController().popBackStack()
@@ -39,8 +61,38 @@ class SelectCityFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        setUpMap()
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun setUpMap() {
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+            requestPermissions(permissions, PERMISSION_CODE)
+
+            return
+        }
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location->
+                if (location != null) {
+                    lastLocation = location
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                    map.addMarker(MarkerOptions().position(currentLatLng).title("Sua região"))
+                }
+            }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1003
+    }
+
+
 }
